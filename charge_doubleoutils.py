08619ced8c27ba2fff5a32aa8bc56d478635b1b3
stardom1957 <jcdmartel@gmail.com>
2023-06-12 22:07:26 -0400

    ajout de charge_doublesoutils

---
+++ charge_doubleoutils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 14:48:43 2021
+
+@author: dominique
+version 05
+"""
+import os
+import sys
+
+# insérer le chemin suivant dans sys.path pour trouver le package astrodm
+if 'D:\DOCUMENTS\Astronomie\dev' not in sys.path:
+    sys.path.insert(0, 'D:\DOCUMENTS\Astronomie\dev')
+from astrodm import doublesoutils as do
